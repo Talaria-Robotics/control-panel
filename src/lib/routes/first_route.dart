8f876c4d829e6f8896696b0935d58e66592cbb0f
Yoshi Askharoun <jjask7@gmail.com>
2024-10-13 18:33:08 -0500

    Add get started button

--- src/lib/routes/first_route.dart
+++ src/lib/routes/first_route.dart
@@ -8,24 +8,33 @@ class FirstRoute extends StatelessWidget {
     return Container(
       color: Theme.of(context).colorScheme.inversePrimary,
       child: Center(
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ConstrainedBox(
-              constraints: const BoxConstraints(
-                maxHeight: 100
-              ),
-              child: Image.asset("assets/images/TalariaLogoSquare.png"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ConstrainedBox(
+                  constraints: const BoxConstraints(
+                      maxHeight: 75
+                  ),
+                  child: Image.asset("assets/images/TalariaLogoSquare.png"),
+                ),
+                Text(
+                  " Talaria",
+                  style: Theme.of(context).textTheme.headlineMedium!
+                      .copyWith(fontWeight: FontWeight.bold, fontSize: 32),
+                ),
+                Text(
+                  " Robotics",
+                  style: Theme.of(context).textTheme.headlineMedium!
+                      .copyWith(fontSize: 32),
+                ),
+              ],
             ),
-            Text(
-              " Talaria",
-              style: Theme.of(context).textTheme.headlineMedium!
-                  .copyWith(fontWeight: FontWeight.bold, fontSize: 32),
-            ),
-            Text(
-              " Robotics",
-              style: Theme.of(context).textTheme.headlineMedium!
-                .copyWith(fontSize: 32),
+            FilledButton(
+              onPressed: () {},
+              child: const Text("Get Started")
             ),
           ],
         )
