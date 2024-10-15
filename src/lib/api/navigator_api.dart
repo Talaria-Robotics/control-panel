4bde5b3d07cec10108f8f9d0be89aa1d073f7605
Yoshi Askharoun <jjask7@gmail.com>
2024-10-15 13:22:04 -0500

    More models

--- src/lib/api/navigator_api.dart
+++ src/lib/api/navigator_api.dart
@@ -1,7 +1,9 @@
 import 'package:control_panel/api/navigator_models.dart';
 
 abstract interface class NavigatorApi {
+  static late final NavigatorApi instance;
+
   Future<PossibleMailRouteInfo> getPossibleRouteInfo();
 
-  Future<void> setRoute()
+  Future<void> setRoute(RequestedMailRoute route);
 }
--- src/lib/api/navigator_models.dart
+++ src/lib/api/navigator_models.dart
@@ -1,6 +1,25 @@
-class PossibleMailRouteInfo {
-  late Iterable<String> roomNames;
-  late String routeName;
+class PossibleMailRouteInfo extends Identified {
+  late Iterable<MailRouteRoom> rooms;
+  late Iterable<MailBin> bins;
 }
 
-class
+class MailRouteRoom extends Identified { }
+
+class MailBin {
+  late int number;
+  late String name;
+}
+
+class MailRouteStop {
+  late String roomId;
+  late int binNumber;
+}
+
+class RequestedMailRoute {
+  late Iterable<MailRouteStop> stops;
+}
+
+interface class Identified {
+  late String id;
+  late String name;
+}
